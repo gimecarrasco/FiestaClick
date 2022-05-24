3e4f99e38d8204a8214018e1005a37a9dc250904
Vanina Godoy <contadora.godoy.vanina@gmail.com>
2022-05-24 20:52:17 -0300

    Entitys by Vani and ErrorService

--- src/main/java/com/fiestaClick/demo/entities/PhotoEntity.java
+++ src/main/java/com/fiestaClick/demo/entities/PhotoEntity.java
@@ -1,6 +1,31 @@
-
 package com.fiestaClick.demo.entities;
 
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Entity
 public class PhotoEntity {
     
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String mime;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] content;
+
+    private boolean alta;
+
 }
