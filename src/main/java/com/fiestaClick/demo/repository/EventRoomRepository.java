eba07f7af5ac9b2205a4a854b81848594d112976
ZAHIRALI79 <zahiramellado1979@gmail.com>
2022-05-27 12:52:54 -0300

    RepoEventRoom

---
+++ src/main/java/com/fiestaClick/demo/repository/EventRoomRepository.java
@@ -0,0 +1,26 @@
+package com.fiestaClick.demo.repository;
+
+import com.fiestaClick.demo.entities.EventRoomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author 54261
+ */
+
+@Repository
+public interface EventRoomRepository extends JpaRepository<EventRoomEntity, String> {
+
+    @Query("SELECT e FROM EventRoomEntity e WHERE e.name = :name")
+    public EventRoomEntity findByName(@Param("name") String name);
+    
+    @Query("SELECT e FROM EvenRoomEntity e WHERE c.city = :city")
+    public EventRoomEntity findByCity(@Param("city") String city);
+    
+    @Query("SELECT e FROM EvenRoomEntity e WHERE c.adress = :adress")
+    public EventRoomEntity findByAdress(@Param("adress") String adress);
+}
+
