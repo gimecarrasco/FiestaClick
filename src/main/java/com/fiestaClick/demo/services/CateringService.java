8cb79a2da97c4166e7fcdb908954d40fedbbe061
YamilaEsteche <yami22est@hotmail.com>
2022-06-01 19:37:21 -0300

    agrego cateringService

--- src/main/java/com/fiestaClick/demo/services/CateringService.java
+++ src/main/java/com/fiestaClick/demo/services/CateringService.java
@@ -5,6 +5,7 @@ import com.fiestaClick.demo.entities.PhotoEntity;
 import com.fiestaClick.demo.errors.ErrorService;
 import com.fiestaClick.demo.repository.CateringRepository;
 import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,19 +45,68 @@ public class CateringService {
         }
     }
     
+     @Transactional
+    public CateringEntity enable(String id) throws ErrorService { //enel ejemplo de perro este metodo devuelve un perro
+        Optional<CateringEntity> respuesta = cateringRepository.findById(id);
+        if (respuesta.isPresent()) {
+            CateringEntity catering = respuesta.get();
+            catering.setRegister(Boolean.TRUE);
+            return cateringRepository.save(catering);
+        } else {
+            throw new ErrorService("No Existe este Catering");
+        }
+    }
     
     
+    @Transactional
+    public CateringEntity disable(String id) throws ErrorService { 
+        Optional<CateringEntity> respuesta = cateringRepository.findById(id);
+        if (respuesta.isPresent()) {
+            CateringEntity catering = respuesta.get();
+            catering.setRegister(Boolean.FALSE);
+            return cateringRepository.save(catering);
+        } else {
+            throw new ErrorService("No Existe este Catering");
+        }
+    }
+    
+     @Transactional
+    public CateringEntity modify(String id, String name, Integer price, String description, PhotoEntity photoEntity) throws ErrorService, Exception{
+         Optional<CateringEntity> respuesta = cateringRepository.findById(id);
+         
+          validate(name, price, description);
+          
+          if (respuesta.isPresent()) {
+            CateringEntity catering = respuesta.get();
+            
+           catering.setName(name);
+           catering.setDescription(description);
+           catering.setPrice(price);
+           catering.setPhotoEntity(photoEntity);
+            
+            return cateringRepository.save(catering);
 
-//    @Transactional
-//    public List<CateringEntity> listCatering() {
-//        return cateringRepository.findAll();
-//    }
-//
-//    @Transactional
-//    public CateringEntity findCateringbyName(String catering) {
-//        return cateringRepository.findCateringbyname(catering);
-//    }
+        } else {
+            throw new ErrorService("No se encontro el catering que se desea modificar");
+        }
+         
+    }
+    
+    @Transactional
+    public List<CateringEntity> listCatering() {
+        return cateringRepository.findAll();
+    }
     
     
+   
+    @Transactional
+    public CateringEntity findCateringByName(String catering) {
+        return cateringRepository.findCateringByName(catering);
+    }
+    
+    @Transactional
+    public void deleteById(String id) {
+        cateringRepository.deleteById(id);
+    }
 
 }
