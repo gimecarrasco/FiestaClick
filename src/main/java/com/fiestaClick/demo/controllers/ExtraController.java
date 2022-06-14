9cb77d8be998819fe3f590f4c1fa8ec62fb47eb0
YamilaEsteche <yami22est@hotmail.com>
2022-06-14 18:29:03 -0300

    mail sender

--- src/main/java/com/fiestaClick/demo/controllers/ExtraController.java
+++ src/main/java/com/fiestaClick/demo/controllers/ExtraController.java
@@ -1,7 +1,8 @@
 
 package com.fiestaClick.demo.controllers;
 
-import com.fiestaClick.demo.services.ExtraServiceService;
+
+import com.fiestaClick.demo.service.ExtraServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
