18533e688e83bef5d3cc7a755f91c559586668a1
Gimena Carrasco <gimecarrasco@gmail.com>
2022-06-14 12:31:30 -0300

    Main controller y login.html

--- src/main/java/com/fiestaClick/demo/controllers/MainController.java
+++ src/main/java/com/fiestaClick/demo/controllers/MainController.java
@@ -1,20 +1,26 @@
 package com.fiestaClick.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(ModelMap modelo) {
+        modelo.put("exito", "Has sido registrado exitosamente.");
         return "index.html";
-    }
-
-    @GetMapping("/login")
-    public String login() {
+    } 
+    
+     @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, ModelMap model) {
+         if (error != null) {
+             model.put("error", "El nombre de usuario o contraseña son incorrectos.");
+         }
         return "login.html";
     }
 
